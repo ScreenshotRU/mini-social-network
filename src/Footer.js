a32803a54ea26654bf5aeb88f961425b3699521c
Fedor <screenshot@inbox.ru>
2020-10-12 17:16:41 +0700

    Delete files

--- src/Footer.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Footer = () => {
-  return <div>screenshot footer</div>;
-};
-
-export default Footer;
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import s from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
   return (
     <div className={s.item}>
       <img
         src='https://www.ejin.ru/wp-content/uploads/2018/10/crew4_1024.png'
         alt=''
       />
-      post
+      {props.message}
       <div>
         <span>like</span>
       </div>
