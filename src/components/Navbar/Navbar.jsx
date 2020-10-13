57ff02ec94a16e59b8c90875cd40bff9cdb5c622
Fedor <screenshot@inbox.ru>
2020-10-13 16:49:49 +0700

    Add navLinks
    lesson 21

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import s from './Navbar.module.css';
 console.log(s);
 
@@ -6,10 +7,14 @@ const Navbar = () => {
   return (
     <nav className={s.nav}>
       <div className={s.item}>
-        <a href='/profile'>Profile</a>
+        <NavLink to='/profile' activeClassName={s.activeLink}>
+          Profile
+        </NavLink>
       </div>
       <div className={`${s.item} ${s.active}`}>
-        <a href='/dialogs'>Messages</a>
+        <NavLink to='/dialogs' activeClassName={s.activeLink}>
+          Messages
+        </NavLink>
       </div>
       <div className={s.item}>
         <a>News</a>
