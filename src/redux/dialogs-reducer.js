204a70accb5e1dc7f739ff418b5e73c42fd57c54
Fedor <screenshot@inbox.ru>
2020-10-31 20:52:54 +0700

    Add redux-store and update reducers (lesson 42)

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -1,7 +1,27 @@
 const UPDATE_NEW_MESSAGE_BODY = "UPDATE_NEW_MESSAGE_BODY";
 const SEND_MESSAGE = "SEND_MESSAGE";
 
-const dialogReducer = (state, action) => {
+let initialState = {
+  dialogs: [
+    { id: 1, name: "Dima" },
+    { id: 2, name: "Sasha" },
+    { id: 3, name: "Sveta" },
+    { id: 4, name: "Valera" },
+    { id: 5, name: "Roma" },
+    { id: 6, name: "Fedya" },
+  ],
+  messages: [
+    { id: 1, message: "Hi" },
+    { id: 2, message: "no" },
+    { id: 3, message: "yes" },
+    { id: 4, message: "do" },
+    { id: 5, message: "homework" },
+    { id: 6, message: "I am" },
+  ],
+  newMessageBody: "",
+};
+
+const dialogReducer = (state = initialState, action) => {
   switch (action.type) {
     case UPDATE_NEW_MESSAGE_BODY:
       state.newMessageBody = action.body;
@@ -18,8 +38,8 @@ const dialogReducer = (state, action) => {
 
 export const sendMessageCreator = () => ({ type: SEND_MESSAGE });
 export const updateNewMessageBodyCreator = (body) => ({
-    type: UPDATE_NEW_MESSAGE_BODY,
-    body: body,
+  type: UPDATE_NEW_MESSAGE_BODY,
+  body: body,
 });
 
 export default dialogReducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -53,9 +53,5 @@ let store = {
   },
 };
 
-
-
-
-
 export default store;
 window.store = store;
