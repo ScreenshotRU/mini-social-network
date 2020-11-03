fff82ea8928d697d94935683c580d17d8c8803b6
Fedor <screenshot@inbox.ru>
2020-11-03 17:32:06 +0700

    Add container components

--- src/index.js
+++ src/index.js
@@ -1,31 +1,25 @@
-import store from './redux/redux-store';
-import * as serviceWorker from './serviceWorker';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import store from "./redux/redux-store";
+import * as serviceWorker from "./serviceWorker";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 export let rerenderEntireTree = (state) => {
   ReactDOM.render(
     <BrowserRouter>
-      <React.StrictMode>
-        <App
-          state={state}
-          dispatch={store.dispatch.bind(store)}
-          store={store}
-        />
-      </React.StrictMode>
+      <App state={state} dispatch={store.dispatch.bind(store)} store={store} />
     </BrowserRouter>,
-    document.getElementById('root')
+    document.getElementById("root")
   );
 };
 
 rerenderEntireTree(store.getState());
 
 store.subscribe(() => {
-    let state = store.getState();
-    rerenderEntireTree(state);
+  let state = store.getState();
+  rerenderEntireTree(state);
 });
 
 // If you want your app to work offline and load faster, you can change
