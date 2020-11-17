dbf5f04cf34f5e27c57bc55f7226d75619c009e6
Fedor <screenshot@inbox.ru>
2020-11-17 16:08:17 +0700

    Add axios and get data

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,46 +1,15 @@
 import React from 'react';
 import styles from './users.module.css';
+import axios from 'axios';
+import userPhoto from '../../assets/images/user.png';
 
 let Users = (props) => {
   if (props.users.length === 0) {
-    props.setUsers([
-      {
-        id: 1,
-        photoUrl:
-          'https://www.clipartmax.com/png/full/97-978328_avatar-icon-free-fa-user-circle-o.png',
-        followed: false,
-        fullName: 'Fedor',
-        status: 'I am a boss',
-        location: { city: 'Krasnoyarsk', country: 'Russia' },
-      },
-      {
-        id: 2,
-        photoUrl:
-          'https://www.clipartmax.com/png/full/97-978328_avatar-icon-free-fa-user-circle-o.png',
-        followed: true,
-        fullName: 'Sasha',
-        status: 'I am a boss, maybe',
-        location: { city: 'Moscow', country: 'Russia' },
-      },
-      {
-        id: 3,
-        photoUrl:
-          'https://www.clipartmax.com/png/full/97-978328_avatar-icon-free-fa-user-circle-o.png',
-        followed: false,
-        fullName: 'Dasha',
-        status: 'I am a boss too',
-        location: { city: 'Minsk', country: 'Belarus' },
-      },
-      {
-        id: 4,
-        photoUrl:
-          'https://www.clipartmax.com/png/full/97-978328_avatar-icon-free-fa-user-circle-o.png',
-        followed: true,
-        fullName: 'Natasha',
-        status: 'I am a boss, but mini',
-        location: { city: 'Kiev', country: 'Ukraine' },
-      },
-    ]);
+    axios
+      .get('https://social-network.samuraijs.com/api/1.0/users')
+      .then((response) => {
+        props.setUsers(response.data.items);
+      });
   }
   return (
     <div>
@@ -48,7 +17,11 @@ let Users = (props) => {
         <div key={u.id}>
           <span>
             <div>
-              <img src={u.photoUrl} alt="" className={styles.usersPhoto} />
+              <img
+                src={u.photos.small != null ? u.photos.small : userPhoto}
+                alt=""
+                className={styles.usersPhoto}
+              />
             </div>
             <div>
               {u.followed ? (
@@ -72,12 +45,12 @@ let Users = (props) => {
           </span>
           <span>
             <span>
-              <div>{u.fullName}</div>
+              <div>{u.name}</div>
               <div>{u.status}</div>
             </span>
             <span>
-              <div>{u.location.country}</div>
-              <div>{u.location.city}</div>
+              <div>{'u.location.country'}</div>
+              <div>{'u.location.city'}</div>
             </span>
           </span>
         </div>
