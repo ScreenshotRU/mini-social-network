c2b3f44230f0dfdb48d5be40a5d27cdeb3699b99
ScreenshotRU <screenshot@inbox.ru>
2021-01-25 18:35:29 +0700

    add auth to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import s from './Header.module.css';
+import { NavLink } from 'react-router-dom';
 
-const Header = () => {
+const Header = (props) => {
   return (
     <header className={s.header}>
-      <img src='http://cdn.onlinewebfonts.com/svg/download_504356.png' alt='' />
+      <img src="http://cdn.onlinewebfonts.com/svg/download_504356.png" alt="" />
+      <div className={s.loginBlock}>
+        {props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink>}
+      </div>
     </header>
   );
 };
