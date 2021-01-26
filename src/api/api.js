1c8c1c4a4ad8626c5525dc1f6f912c5f6d728b88
ScreenshotRU <screenshot@inbox.ru>
2021-01-26 09:41:45 +0700

    add api.js and refact some code

---
+++ src/api/api.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+s;
+const instance = axios.create({
+  withCredentials: true,
+  baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+  headers: {
+    'API-KEY': 'cfea807b-84a3-4e5e-b7e6-c3f756d151b3',
+  },
+});
+
+export const usersAPI = {
+  getUsers(currentPage = 1, pageSize = 10) {
+    return instance
+      .get(`users?page=${currentPage}&count=${pageSize}`)
+      .then((response) => {
+        return response.data;
+      });
+  },
+};
+
+export const getUsers2 = (currentPage = 1, pageSize = 10) => {
+  return instance
+    .get(`follow?page=${currentPage}&count=${pageSize}`)
+    .then((response) => {
+      return response.data;
+    });
+};
