c3f35fbc3c90dd68709311b8a82863dad88e0463
ScreenshotRU <screenshot@inbox.ru>
2021-01-28 14:31:22 +0700

    refact Profile & Header components to thunk

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,22 +1,17 @@
 import React from 'react';
 import Profile from './Profile';
-import axios from 'axios';
-import { setUserProfile } from '../../redux/profile-reducer';
+import { getUserProfile } from '../../redux/profile-reducer';
 import { connect } from 'react-redux';
-import {withRouter} from "react-router";
+import { withRouter } from 'react-router';
+
 
 class ProfileContainer extends React.Component {
   componentDidMount() {
-
     let userId = this.props.match.params.userId;
     if (!userId) {
       userId = 2;
     }
-    axios
-      .get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId)
-      .then((response) => {
-        this.props.setUserProfile(response.data);
-      });
+    this.props.getUserProfile(userId)
   }
 
   render() {
@@ -30,4 +25,6 @@ let mapStateToProps = (state) => ({
 
 let WithUrlDataContainerComponent = withRouter(ProfileContainer);
 
-export default connect(mapStateToProps, { setUserProfile })(WithUrlDataContainerComponent);
+export default connect(mapStateToProps, { getUserProfile })(
+  WithUrlDataContainerComponent
+);
